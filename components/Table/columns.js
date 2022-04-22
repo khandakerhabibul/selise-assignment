507d2b796e0725e08076c8ff99f8526f034715f5
Khandaker Amin <khandaker@teenybeans.in>
2022-04-22 23:04:36 +0600

    feat: added customized column design

--- components/Table/columns.js
+++ components/Table/columns.js
@@ -1,5 +1,12 @@
 export const tableColumn = [
-  { Header: 'ID', accessor: 'id' },
+  {
+    Header: 'ID',
+    accessor: 'id',
+    Cell: (val) => {
+      const { id } = val.row.original;
+      return <div style={{ width: 120, background: 'red' }}>{id}</div>;
+    },
+  },
   { Header: 'Name', accessor: 'name' },
   { Header: 'District', accessor: 'district' },
 ];
